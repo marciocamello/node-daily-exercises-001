3d88500e76f4d374bf360a8e262d16e434462922
Marcio Vale <mvale@smartdigit.pt>
2023-09-07 10:12:49 +0100

    first commit

---
+++ createTable.js
@@ -0,0 +1,13 @@
+import { sql } from './db.js';
+
+sql`DROP TABLE IF EXISTS videos;`.then(() => console.log('Table removed successfully!'))
+
+sql`
+    CREATE TABLE videos (
+        id TEXT PRIMARY KEY,
+        title TEXT,
+        description TEXT,
+        duration INTEGER
+    );
+`.then(() => console.log('Table created successfully!'))
+
